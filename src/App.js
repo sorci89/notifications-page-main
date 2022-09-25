da91c279cffcb3f63127f541d6f22dc484bfeda4
sorci89 <sore.eszter@gmail.com>
2022-09-25 22:42:08 +0200

    create and render notifications database

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
-
+import { notifications } from "./Data/notifications";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>Notifications</p>
+      {notifications.map((notification) => (
+        <div>
+          <div>
+            <img src={notification.userProfile} alt="User profile" />
+          </div>
+          <span>{notification.userName}</span>{" "}
+          <span>{notification.action}</span>{" "}
+          {notification.type === "withLink" && <span>{notification.link}</span>}
+          <p>{notification.time}</p>
+          {notification.type === "withMessage" && <p>{notification.message}</p>}
+          {notification.type === "withPicture" && (
+            <div>
+              <img src={notification.picture} alt="User profile" />
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
